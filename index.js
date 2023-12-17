4cc72d16a7a9bf1ea7578544180cd70a2bae46d0
Nasretulla <folad8@hotmail.com>
2023-12-17 13:04:15 +0200

    3.5: puhelinluettelon backend step5 adding new person

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const app = express();
 
@@ -38,7 +39,7 @@ app.get("/info", (req, res) => {
     res.send(`Phonebook has info for ${persons.length} people   \n${new Date()}`);
   });
 
-
+// find person with given ID
   app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const personID = persons.find(personID => personID.id === id)
@@ -52,13 +53,41 @@ app.get("/info", (req, res) => {
 
 })
 
-
+//Delete person with given ID
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(persons => persons.id !== id)
 
     response.status(204).end()
 })
+
+// Adding a person
+const generateId = () => {
+    const maxId = persons.length > 0 
+    ? Math.max(...persons.map ( n => n.id)) : 0
+    return maxId +5
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body.content) {
+        return response.status(404).json({
+            error: 'content missing'
+        })
+    }
+
+    const  personToAdd = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+
+    }
+    persons = persons.concat(personToAdd)
+    console.log(personToAdd)
+    response.json(personToAdd)
+})
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
